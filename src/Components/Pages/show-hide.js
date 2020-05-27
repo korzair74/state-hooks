0ac8e2a0ab348707e325b32cd69c079f5445ecc7
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-27 14:01:49 -0400

    show hide page complete, updated style for global use

--- src/Components/Pages/show-hide.js
+++ src/Components/Pages/show-hide.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 export default class ShowHide extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			show: false,
+		};
+	}
+
+	toggle = () => {
+		this.setState({
+			show: !this.state.show,
+		});
+	};
+
 	render() {
 		return (
-			<div>
-				<h1>Show Hide Component</h1>
+			<div className='button-container'>
+				<h1>Show and Hide</h1>
+				<div className='button-wrapper'>
+					<button className='btn' onClick={this.toggle}>
+						{this.state.show === true ? 'Show' : 'Hide'}
+					</button>
+				</div>
+				<h1>{this.state.show === true ? 'Show' : ''}</h1>
 			</div>
 		);
 	}
