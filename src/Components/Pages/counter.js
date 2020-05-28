db8c3d5601594b9afbea02f3102e560495ab020e
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-28 11:44:10 -0400

    Completed counter

--- src/Components/Pages/counter.js
+++ src/Components/Pages/counter.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Counter extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Counter Component</h1>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  countUp = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+  countDown = (event) => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div className='button-container'>
+        <h1>Counter</h1>
+        <div className='button-wrapper'>
+          <button className='btn' onClick={this.countUp}>
+            +
+          </button>
+          <button className='btn' onClick={this.countDown}>
+            -
+          </button>
+        </div>
+        <h1>{this.state.count}</h1>
+      </div>
+    );
+  }
 }
--- src/Components/Pages/show-hide.js
+++ src/Components/Pages/show-hide.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ShowHide extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			show: false,
-		};
-	}
+    this.state = {
+      show: false,
+    };
+  }
 
-	toggle = () => {
-		this.setState({
-			show: !this.state.show,
-		});
-	};
+  toggle = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
 
-	render() {
-		return (
-			<div className='button-container'>
-				<h1>Show and Hide</h1>
-				<div className='button-wrapper'>
-					<button className='btn' onClick={this.toggle}>
-						{this.state.show === true ? 'Show' : 'Hide'}
-					</button>
-				</div>
-				<h1>{this.state.show === true ? 'Show' : ''}</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='button-container'>
+        <h1>Show and Hide</h1>
+        <div className='button-wrapper'>
+          <button className='btn' onClick={this.toggle}>
+            {this.state.show ? "Show" : "Hide"}
+          </button>
+        </div>
+        <h1>{this.state.show ? "Show" : ""}</h1>
+      </div>
+    );
+  }
 }
